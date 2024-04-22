48b3e09a4c8ef7faa776e70377580a76116cfc30
vekosek <88042533+VictoriaKlyueva@users.noreply.github.com>
2024-04-23 00:15:34 +0700

    Добавлены основные классы и визуальная часть

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace HitsZoo
+﻿using System;
+
+namespace HitsZoo
 {
     partial class Form1
     {
@@ -28,20 +30,221 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.horseButton = new System.Windows.Forms.Button();
+            this.capybaraButton = new System.Windows.Forms.Button();
+            this.barsButton = new System.Windows.Forms.Button();
+            this.staffButton = new System.Windows.Forms.Button();
+            this.visitorButton = new System.Windows.Forms.Button();
+            this.addEntity = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.textBoxVoice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // horseButton
+            // 
+            this.horseButton.Location = new System.Drawing.Point(13, 45);
+            this.horseButton.Margin = new System.Windows.Forms.Padding(4);
+            this.horseButton.Name = "horseButton";
+            this.horseButton.Size = new System.Drawing.Size(147, 36);
+            this.horseButton.TabIndex = 0;
+            this.horseButton.Text = "Казахский конь";
+            this.horseButton.UseVisualStyleBackColor = true;
+            // 
+            // capybaraButton
+            // 
+            this.capybaraButton.Location = new System.Drawing.Point(323, 45);
+            this.capybaraButton.Margin = new System.Windows.Forms.Padding(4);
+            this.capybaraButton.Name = "capybaraButton";
+            this.capybaraButton.Size = new System.Drawing.Size(147, 36);
+            this.capybaraButton.TabIndex = 1;
+            this.capybaraButton.Text = "Капибара";
+            this.capybaraButton.UseVisualStyleBackColor = true;
+            this.capybaraButton.Click += new System.EventHandler(this.capybaraButton_Click);
+            // 
+            // barsButton
+            // 
+            this.barsButton.Location = new System.Drawing.Point(168, 45);
+            this.barsButton.Margin = new System.Windows.Forms.Padding(4);
+            this.barsButton.Name = "barsButton";
+            this.barsButton.Size = new System.Drawing.Size(147, 36);
+            this.barsButton.TabIndex = 2;
+            this.barsButton.Text = "Кыргызский барс";
+            this.barsButton.UseVisualStyleBackColor = true;
+            // 
+            // staffButton
+            // 
+            this.staffButton.Location = new System.Drawing.Point(478, 45);
+            this.staffButton.Margin = new System.Windows.Forms.Padding(4);
+            this.staffButton.Name = "staffButton";
+            this.staffButton.Size = new System.Drawing.Size(147, 36);
+            this.staffButton.TabIndex = 3;
+            this.staffButton.Text = "Сотрудник";
+            this.staffButton.UseVisualStyleBackColor = true;
+            this.staffButton.Click += new System.EventHandler(this.staffButton_Click);
+            // 
+            // visitorButton
+            // 
+            this.visitorButton.Location = new System.Drawing.Point(633, 45);
+            this.visitorButton.Margin = new System.Windows.Forms.Padding(4);
+            this.visitorButton.Name = "visitorButton";
+            this.visitorButton.Size = new System.Drawing.Size(147, 36);
+            this.visitorButton.TabIndex = 4;
+            this.visitorButton.Text = "Посетитель";
+            this.visitorButton.UseVisualStyleBackColor = true;
+            // 
+            // addEntity
+            // 
+            this.addEntity.AutoSize = true;
+            this.addEntity.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.addEntity.Location = new System.Drawing.Point(13, 9);
+            this.addEntity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.addEntity.Name = "addEntity";
+            this.addEntity.Size = new System.Drawing.Size(251, 29);
+            this.addEntity.TabIndex = 5;
+            this.addEntity.Text = "Добавить сущность:";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.timeLabel.Location = new System.Drawing.Point(1152, 9);
+            this.timeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(97, 29);
+            this.timeLabel.TabIndex = 6;
+            this.timeLabel.Text = "Время:";
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.time.Location = new System.Drawing.Point(1189, 44);
+            this.time.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(27, 29);
+            this.time.TabIndex = 7;
+            this.time.Text = "0";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(564, 88);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(216, 22);
+            this.textBoxName.TabIndex = 8;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(564, 116);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(216, 22);
+            this.textBoxAge.TabIndex = 9;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(484, 94);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(33, 16);
+            this.nameLabel.TabIndex = 11;
+            this.nameLabel.Text = "Имя";
+            this.nameLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Location = new System.Drawing.Point(484, 122);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(62, 16);
+            this.ageLabel.TabIndex = 12;
+            this.ageLabel.Text = "Возраст";
+            // 
+            // textBoxVoice
+            // 
+            this.textBoxVoice.Location = new System.Drawing.Point(76, 88);
+            this.textBoxVoice.Name = "textBoxVoice";
+            this.textBoxVoice.Size = new System.Drawing.Size(394, 22);
+            this.textBoxVoice.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Голос";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1100, 761);
+            this.ClientSize = new System.Drawing.Size(1262, 802);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxVoice);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.addEntity);
+            this.Controls.Add(this.visitorButton);
+            this.Controls.Add(this.staffButton);
+            this.Controls.Add(this.barsButton);
+            this.Controls.Add(this.capybaraButton);
+            this.Controls.Add(this.horseButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "HITS Zoo";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button horseButton;
+        private System.Windows.Forms.Button capybaraButton;
+        private System.Windows.Forms.Button barsButton;
+        private System.Windows.Forms.Button staffButton;
+        private System.Windows.Forms.Button visitorButton;
+        private System.Windows.Forms.Label addEntity;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.TextBox textBoxVoice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -7,6 +8,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Numerics;
+using System.Windows;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
 
 namespace HitsZoo
 {
@@ -15,6 +20,154 @@ namespace HitsZoo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Zoo zoo = new Zoo();
+        int currentTime = 0;
+
+        // обработчик события Tick таймера
+        void timer_Tick(object sender, EventArgs e)
+        {
+            currentTime += 1;
+            time.Text = currentTime.ToString();
+            zoo.updateAnimals();
+
+            zoo.printAnimals();
+            zoo.printPersons();
+        }
+
+        // Создание капибары
+        private void capybaraButton_Click(object sender, EventArgs e)
+        {   
+            string voice = textBoxVoice.Text;
+            Animal currentAnimal = new Animal(voice);
+            zoo.addAnimal(currentAnimal);
+        }
+
+        // Создание работника
+        private void staffButton_Click(object sender, EventArgs e)
+        {
+            string name = textBoxName.Text;
+            string age = textBoxAge.Text;
+            Person currentPerson = new Person(name, Convert.ToInt32(age));
+            zoo.addPerson(currentPerson);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Zoo
+    {
+        int animalsCount = 0;
+        Animal[] animalsArray = new Animal[100];
+        int personsCount = 0;
+        Person[] personsArray = new Person[100];
+
+        public void addAnimal(Animal animal)
+        {
+            animalsArray[animalsCount] = animal;
+            animalsCount++;
+        }
+
+        public void addPerson(Person human)
+        {
+            personsArray[personsCount] = human;
+            personsCount++;
+        }
+
+        public void updateAnimals()
+        {
+            for (int i = 0; i < animalsCount; i++)
+            {
+                animalsArray[i].currentHunger += 1;
+                animalsArray[i].update();
+            }
+        }
+
+        public void printAnimals()
+        {
+            for (int i = 0; i < animalsCount; i++)
+            {
+                Console.WriteLine($"Номер: {i}");
+                animalsArray[i].print();
+            }
+        }
+
+        public void printPersons()
+        {
+            for (int i = 0; i < personsCount; i++)
+            {
+                Console.WriteLine($"Номер: {i}");
+                personsArray[i].print();
+            }
+        }
+    }
+
+    public class Animal
+    {
+        int criticalHunger = new Random().Next(10, 20);
+        public int currentHunger = 0;
+        public bool isHungry = false;
+        string voice = "meow";
+
+        public void update()
+        {
+            if (currentHunger > criticalHunger)
+            {
+                isHungry = true;
+            }
+        }
+
+        public void feed()
+        {
+            currentHunger = 0;
+            isHungry = false;
+        }
+
+        public void submitVote()
+        {
+            Console.WriteLine(voice);
+        }
+
+        public void print()
+        {
+            Console.WriteLine($"Численный голод: {currentHunger}  " +
+                              $" Голод: {isHungry}");
+        }
+
+        public Animal(string voice) 
+        {
+            this.voice = voice;
+        }
+    }
+
+    public class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(name + " " + age);
         }
     }
 }
